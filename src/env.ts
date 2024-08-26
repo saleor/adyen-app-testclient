de4e7775e7907cbff4eeba75242f1ee8a7b47690
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-08-26 08:19:24 +0200

    Add tslog (#12)

--- src/env.ts
+++ src/env.ts
@@ -9,11 +9,16 @@ export const env = createEnv({
       .url()
       .refine((v) => v.endsWith("/graphql/"), "Must end with /graphql/"),
     NEXT_PUBLIC_INITIAL_CHANNEL_SLUG: z.string(),
+    NEXT_PUBLIC_LOG_LEVEL: z
+      .enum(["info", "warn", "error"])
+      .optional()
+      .default("info"),
   },
   runtimeEnv: {
     NEXT_PUBLIC_INITIAL_CHANNEL_SLUG:
       process.env.NEXT_PUBLIC_INITIAL_CHANNEL_SLUG,
     NEXT_PUBLIC_INITIAL_ENV_URL: process.env.NEXT_PUBLIC_INITIAL_ENV_URL,
+    NEXT_PUBLIC_LOG_LEVEL: process.env.NEXT_PUBLIC_LOG_LEVEL,
   },
   extends: [vercel()],
   isServer: typeof window === "undefined",
