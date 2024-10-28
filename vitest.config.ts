cc6385ec152699443f83c74fd66934c78871a59d
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-10-28 09:24:28 +0100

    Remove not used plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,13 @@
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   test: {
     environment: "jsdom",
     watch: false,
+    alias: {
+      "@/": new URL("./src/", import.meta.url).pathname,
+    },
   },
 });
