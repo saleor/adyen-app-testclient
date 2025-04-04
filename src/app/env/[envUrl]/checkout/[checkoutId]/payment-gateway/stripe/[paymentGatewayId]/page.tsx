91e7c4ba612d4ee6c3a00c3eaba5bb891fa88cba
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-04-04 10:51:55 +0200

    actions - fetching pk from gateway initialize

--- src/app/env/[envUrl]/checkout/[checkoutId]/payment-gateway/stripe/[paymentGatewayId]/page.tsx
+++ src/app/env/[envUrl]/checkout/[checkoutId]/payment-gateway/stripe/[paymentGatewayId]/page.tsx
@@ -1,3 +1,21 @@
-export default function StripeDropinPage() {
-  return <div>dupa</div>;
+import { initializePaymentGateway } from "@/modules/stripe/actions/initalize-payment-gateway";
+
+export default async function StripeDropinPage({
+  params: { envUrl, checkoutId, paymentGatewayId },
+}: {
+  params: { envUrl: string; checkoutId: string; paymentGatewayId: string };
+}) {
+  const decodedEnvUrl = decodeURIComponent(envUrl);
+  const decodedPaymentGatewayId = decodeURIComponent(paymentGatewayId);
+
+  const initializedStripeData = await initializePaymentGateway({
+    checkoutId,
+    envUrl: decodedEnvUrl,
+    paymentGatewayId: decodedPaymentGatewayId,
+  });
+
+  // @ts-ignore
+  const publishableKey = initializedStripeData?.data.publishableKey as string;
+
+  return <div>{publishableKey}</div>;
 }
---
+++ src/modules/stripe/actions/initalize-payment-gateway.ts
@@ -0,0 +1,98 @@
+"use server";
+import { graphql } from "gql.tada";
+import request from "graphql-request";
+import { z } from "zod";
+
+import { envUrlSchema } from "@/lib/env-url";
+import { BaseError, UnknownError } from "@/lib/errors";
+import { actionClient } from "@/lib/safe-action";
+
+const InitalizePaymentGatewayMutation = graphql(`
+  mutation initalizePaymentGateway(
+    $checkoutId: ID!
+    $paymentGatewayId: String!
+    $amount: PositiveDecimal
+    $data: JSON
+  ) {
+    paymentGatewayInitialize(
+      id: $checkoutId
+      paymentGateways: [{ id: $paymentGatewayId, data: $data }]
+      amount: $amount
+    ) {
+      gatewayConfigs {
+        id
+        data
+        errors {
+          field
+          message
+          code
+        }
+      }
+      errors {
+        field
+        message
+        code
+      }
+    }
+  }
+`);
+
+// const InitalizePaymentGatewayParsingResponseError = BaseError.subclass(
+//   "InitalizePaymentGatewayParsingResponseError",
+// );
+const InitalizePaymentGatewayMutationError = BaseError.subclass(
+  "InitalizePaymentGatewayMutationError",
+);
+
+export const initializePaymentGateway = actionClient
+  .schema(
+    z.object({
+      checkoutId: z.string(),
+      envUrl: envUrlSchema,
+      paymentGatewayId: z.string(),
+      amount: z.number().optional(), // todo check if this is required for stripe?
+      data: z.any(), // todo
+    }),
+  )
+  .metadata({
+    actionName: "initializePaymentGateway",
+  })
+  .action(
+    async ({
+      parsedInput: { envUrl, checkoutId, paymentGatewayId, amount, data },
+    }) => {
+      const response = await request(envUrl, InitalizePaymentGatewayMutation, {
+        checkoutId,
+        paymentGatewayId,
+        amount,
+        data,
+      }).catch((error) => {
+        throw BaseError.normalize(error, UnknownError);
+      });
+
+      // todo maybe parse to validate response
+
+      const config =
+        response.paymentGatewayInitialize?.gatewayConfigs &&
+        response.paymentGatewayInitialize?.gatewayConfigs[0];
+
+      if (!config) {
+        throw new InitalizePaymentGatewayMutationError(
+          "Failed to initalize payment gateway - errors in initalizePaymentGateway mutation",
+        );
+      }
+
+      if ((config.errors ?? []).length > 0) {
+        throw new InitalizePaymentGatewayMutationError(
+          "Failed to initalize payment gateway - errors in initalizePaymentGateway mutation",
+          {
+            errors: config.errors?.map((e) =>
+              InitalizePaymentGatewayMutationError.normalize(e),
+            ),
+          },
+        );
+      }
+
+      return config.data;
+    },
+  );
