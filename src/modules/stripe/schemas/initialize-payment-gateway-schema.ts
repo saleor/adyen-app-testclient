b8e3f583663a28e6c05383c7414f1882e82d1c97
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-04-09 16:01:45 +0200

    remove not needed file

--- src/modules/stripe/schemas/initialize-payment-gateway-schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const initializePaymentGatewayResponseSchema = z.object({
-  id: z.string(),
-  data: z.object({
-    stripePublishableKey: z.string(),
-  }),
-});
